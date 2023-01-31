d42d9d84a4334ac2f054f7dfa1dbbee0f61b7e1a
Abby <abby.e.smith.85@gmail.com>
2023-01-30 21:37:31 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
+  let result
+  if (someValue>2500)
+    return "No can do."
+  else if (someValue>2000)
+    return "I will gladly take your thirty bucks."  
+  else if (someValue>400)
+    return "That will be twenty bucks."
+  else if (someValue<=400)
+    return "This one is on me!"
   // Write your code here!
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city==='NYC' ? "Ok, sounds good."
+  :  "No go.";
+  //(city=`NYC` ? 'No go.' : 'Ok, sounds good.')
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
+switch (tip){
+  case 'generous':
+    return 'Thank you so much.';
+    break;
+  case 'not as generous':
+    return 'Thank you.'
+    break;
+  default:
+    return 'Bye.';
   // Write your code here!
+  }
 }
